d49f3e7f3c7b170fea7b6f16905e83c14ffa36cd
kwu <kwu@spidersens.com>
2023-11-28 23:19:40 +0800

    update

--- build/plugin/index.js
+++ build/plugin/index.js
@@ -7,17 +7,29 @@ import vue from '@vitejs/plugin-vue'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // rollup打包分析插件
-import {visualizer} from 'rollup-plugin-visualizer'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 import { configHtmlPlugin } from './html'
 import { unocss } from './unocss'
+
+/**
+ * * 组件库按需引入插件
+ * usage: 直接使用组件,无需在任何地方导入组件
+ */
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
+import { configMockPlugin } from './mock'
 
 export function createVitePlugins(viteEnv, isBuild) {
   const plugins = [
     vue(),
     VueSetupExtend(),
     configHtmlPlugin(viteEnv, isBuild),
-    unocss()
+    unocss(),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
   ]
 
   if (isBuild) {
@@ -26,8 +38,12 @@ export function createVitePlugins(viteEnv, isBuild) {
         open: true,
         gzipSize: true,
         brotliSize: true,
-      })
+      }),
     )
+  }
+
+  if (viteEnv?.VITE_APP_USE_MOCK) {
+    plugins.push(configMockPlugin(isBuild))
   }
 
   return plugins
